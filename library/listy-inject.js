1e6fadf80d2f8fbf4bb5af96c501befebdc9efb7
Siva <sivagnanam.student@gmail.com>
2015-06-23 17:56:16 +0545

    script inject file added

---
+++ library/listy-inject.js
@@ -0,0 +1,4 @@
+alert("test1");
+div = '<button id="button" class="  u-sizeFull js-tooltip btn primary-btn tweet-action tweet-btn" type="button">to list</button>'
+$(".ProfileHeaderCard").append(div);
+alert("test");
