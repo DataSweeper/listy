1560bedfd5134650f4734b1d6aecfd6eb50f3122
sivagnanam <gnanamsiva@outlook.com>
2015-10-26 21:39:07 +0530

    clear all

--- Readme.txt
+++
@@ -1,2 +0,0 @@
-Listy 0.1.0
-Listy is a Chrome extension that will allow you to add a twitter user to your own twitter list right from the users twitter page.
--- library/listy-inject.js
+++
@@ -1,8 +0,0 @@
-//div = '<button id="button" class="  u-sizeFull js-tooltip btn primary-btn tweet-action tweet-btn" type="button">to list</button>'
-//$(".ProfileHeaderCard").append(div);
-
-chrome.extension.sendMessage({
-    action: "getSource",
-    source: $(".ProfileHeaderCard-screennameLink").text()
-});
-
